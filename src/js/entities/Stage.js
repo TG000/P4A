1dc4760e980f6ff502dcd9a39e2ec231502ac5b3
TG000 <tranlangiac@gmail.com>
2023-07-06 07:11:17 +0700

    Fixed

--- src/js/entities/Stage.js
+++ src/js/entities/Stage.js
@@ -1,11 +1,7 @@
 export class Stage {
     constructor() {
         this.image = document.createElement('img');
-<<<<<<< HEAD
         this.image.src = "assets/Backgrounds/bg131_02.png";
-=======
-        this.image.src = "../../assets/Backgrounds/bg131_02.png";
->>>>>>> 9c9cb93e4c3491c559735c5aa90b33b5036b8f2d
     }
 
     update() {
